cc98aded49855cf0059646c871219fa629995f65
Paola Maristell Begazo Gonzales <paobegon1@gmail.com>
2016-05-25 11:06:48 -0500

    vamooo

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get("/", function(req, res){
-	res.sendFile(__dirname + "/public/index.html");
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
 });
+app.use(express.static("public"));
 
-io.on("connection", function(socket){
-	console.log("Tenemos una nueva victima!!");
-
-	socket.on("chat", function(_msg){
-		io.emit("nuevo_mensaje", _msg);
+io.on('connection', function(socket)
+{
+	console.log('Tenemos una nueva victima!!');
+	socket.on('chat', function(_msg){
+		io.emit('nuevo_mensaje', _msg);
 	});
-
 });
 
 
-http.listen(8080, function() {
-	console.log("Express run on port 8080");
+
+
+
+
+
+
+
+http.listen(8080, function () {
+	console.log('Muy bien!, eres un crack, sabes trabajar con node JS');
 });
